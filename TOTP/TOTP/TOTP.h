81a8b8b8e4c0e4071ab2e22ff1bb818e98420c95
Hozefa Indorewala <hozefa.indorewala@lock8.me>
2016-09-15 12:03:02 +0200

    Version 0.1.0.

---
+++ TOTP/TOTP/TOTP.h
@@ -0,0 +1,17 @@
+//
+//  TOTP.h
+//  TOTP
+//
+//  Created by Hozefa Indorewala on 15/09/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TOTP.
+FOUNDATION_EXPORT double TOTPVersionNumber;
+
+//! Project version string for TOTP.
+FOUNDATION_EXPORT const unsigned char TOTPVersionString[];
+
+#import <TOTP/TOTPGenerator.h>
